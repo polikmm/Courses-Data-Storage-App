f290cacbe1facc7930efceff371b0fa453ce1858
polikmm <polikmm@mail.ru>
2025-06-10 17:50:06 +0400

    refactor: split request logic in separate files

--- src/components/CourseMeta/CourseActions/DeleteButton.tsx
+++ src/components/CourseMeta/CourseActions/DeleteButton.tsx
@@ -3,18 +3,24 @@ import type { AppDispatch } from "../../../store";
 import styles from "./styles.module.css";
 import icon from "../../../assets/icons/trash.svg";
 import { removeCourse } from "../../../store/coursesSlice/coursesSlice";
+import { deleteCourse } from "../../../api/deleteCourse";
 
 export function DeleteButton({ id }: { id: string }) {
   const dispatch = useDispatch<AppDispatch>();
 
-  const deleteCourse = () => {
-    dispatch(removeCourse(id));
+  const handleDeleteCourse = async () => {
+    const response = await deleteCourse(id);
+    if (response.ok) {
+      dispatch(removeCourse(id));
+    } else {
+      console.error(response.statusText);
+    }
   }
 
   return (
     <button
       className={`${styles.courseCardDeleteButton} button`}
-      onClick={deleteCourse}
+      onClick={handleDeleteCourse}
       aria-label="delete the course"
     >
       <img
